0f6a6e86534350c266431f172e3fe694293f3ec8
Djalma Manfrin <djalma.manfrin@rentcars.com>
2018-09-11 16:54:05 -0300

    handsontable functionality

--- main.js
+++ main.js
@@ -1,15 +1,77 @@
-var data = [
-    ["", "Tesla", "Volvo", "Toyota", "Honda"],
-    ["2017", 10, 11, 12, 13],
-    ["2018", 20, 11, 14, 13],
-    ["2019", 30, 15, 12, 13]
+var typeNumber = {
+        pattern: '0.00',
+        culture: 'pt-BR',
+        thousandSepareted: false
+    },
+    validatorRentalCode = function (value, callback) {
+        if (value && /^[\w]*$/.test(value)) {
+            callback(true);
+        } else {
+            callback(false);
+        }
+    },
+    justTwoDigits = function (value, callback) {
+        if (value && /^[\d]{2}$/.test(value)) {
+            callback(true);
+        } else {
+            callback(false);
+        }
+    },
+    // collorSucess = "#739E73",
+    // collorError = "#CC7878",
+    // linhasProcessadas = 0,
+    numInicialLinhasValidar = 30,
+    blocked = true,
+    div = document.querySelector('#handsontable');
+// save = document.querySelector("#btn_enviar_dados_planilha"),
+
+var colHeaders = [
+    "Código da Locadora",
+    "Valor Comissionável",
+    "Alíquota de Comissão",
+    "Total de Diarias",
+    "Horas Extras",
 ];
 
-var options = {
-    data: data,
-    rowHeaders: true,
-    colHeaders: true
+var columns = [
+    {data: 'cod_locadora', validator: validatorRentalCode, allowEmpty: false, allowInvalid: false},
+    {data: 'valor_comissionavel', type: 'numeric', numericFormat: typeNumber, allowEmpty: false},
+    {data: 'aliquota_rentcars', validator: justTwoDigits, allowEmpty: false, allowInvalid: false},
+    {data: 'total_diarias', type: 'numeric', numericFormat: justTwoDigits, allowEmpty: false, allowInvalid: false},
+    {data: 'horas_extras', type: 'time', timeFormat: 'hh:mm', correctFormat: true, allowEmpty: false},
+];
+
+var validateCells = function() {
+    blocked = false;
+    if (table) {
+        if (table.countRows() < numInicialLinhasValidar) {
+            table.validateCells();
+        } else {
+            table.validateRows(Array.from(new Array(numInicialLinhasValidar).keys()));
+        }
+    }
 };
 
-var container = document.getElementById('example');
-var hot = new Handsontable(container, options);
+var options = {
+    startRows: 0,
+    columns: columns,
+    stretchH: 'all',
+    autoWrapRow: true,
+    height: 545,
+    afterLoadData: validateCells,
+    minSpareRows: true,
+    rowHeaders: true,
+    colHeaders: colHeaders,
+    readOnly: false,
+    manualRowResize: true,
+    manualColumnResize: true,
+    contextMenu: ['copy', 'cut', 'remove_row', 'row_above', 'row_below'],
+    language: 'pt-BR',
+    cells: function () {
+        var cellProperties = {};
+        cellProperties.readOnly = (blocked === true);
+        return cellProperties;
+    }
+};
+
+var table = new Handsontable(div, options);
